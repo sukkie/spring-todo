ff714036f8900a513062cd318eb8cb19fd487b20
sukkie <sukkie512@yahoo.co.jp>
2023-10-08 20:29:09 +0900

    first commit

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -0,0 +1,34 @@
+package com.in28minutes.rest.webservices.restfulwebservices.todo;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class TodoResource {
+
+    private TodoService todoService;
+
+    @GetMapping("/users/{username}/todos")
+    public List<Todo> retrieveTodos(@PathVariable String username) {
+        return todoService.findByUsername(username);
+    }
+
+    @GetMapping("/users/{username}/todo/{id}")
+    public Todo retrieveTodo(@PathVariable String username, @PathVariable int id) {
+        return todoService.findById(id);
+    }
+
+    @DeleteMapping("/users/{username}/todo/{id}")
+    public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable int id) {
+        todoService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
